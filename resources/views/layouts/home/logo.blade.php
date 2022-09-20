a6559ccf67d973e948cf46c3e982a89b394da502
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-09-20 12:23:07 -0600

    Cambio de logo se usa el MAGO

--- resources/views/layouts/home/logo.blade.php
+++ resources/views/layouts/home/logo.blade.php
@@ -2,7 +2,7 @@
 <div class="navbar-brand-box text-center">
     <a href="{{route('dashboard')}}" class="logo logo-light">
         {{-- Clase: avatar-xx md=mediano sm=pequeño  xs=Extra pequeño lg=Largo xl=Extra largo --}}
-        <img  src="{{asset('images/logo.png')}}" class="rounded-circle avatar-xs">
+        <img  src="{{asset('images/mago.png')}}" class="rounded-circle avatar-lg">
 
     </a>
 </div>
