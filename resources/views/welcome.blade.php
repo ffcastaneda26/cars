fc5b65d46828fa0567f711cd83633c3eda2123de
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-09-20 14:23:26 -0600

    Welcome Poner imagen de fondo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,13 +15,16 @@
         </style>
 
         <style>
-            body {
+             body {
                 font-family: 'Nunito', sans-serif;
+                heigh:665px;
+                background-image: url("/images/fondo_01.png");
+                background-size: 10rem;
             }
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <div class="relative flex items-top justify-center min-h-screen  dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
@@ -38,6 +41,9 @@
 
             <img src="{{asset('images/logo.png')}}" alt="">
 
-        </div>
+
     </body>
+
+
+</body>
 </html>
