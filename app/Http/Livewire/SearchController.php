1ad246ea05e40dc7022b88f300c1f223df5c57e8
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-12-15 16:35:16 -0700

    Depuracion de SearchController

--- app/Http/Livewire/SearchController.php
+++ app/Http/Livewire/SearchController.php
@@ -3,24 +3,15 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-use App\Traits\UserTrait;
-use Livewire\WithPagination;
 use App\Http\Livewire\Traits\CrudTrait;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class SearchController extends Component
 {
-    use AuthorizesRequests;
-    use WithPagination;
     use CrudTrait;
-    use UserTrait;
-
 
     public function mount()
     {
         $this->manage_title     = __('Search') . ' ' . __('Vehicles');
-        $this->search_label     = __('Make,Model,Year....');
-
     }
 
     /*+---------------------------------+
@@ -30,11 +21,8 @@ class SearchController extends Component
 
     public function render()
     {
-
         return view('livewire.search.search-controller')->layout('layouts.search_template');
-
     }
-
 
 }
 
