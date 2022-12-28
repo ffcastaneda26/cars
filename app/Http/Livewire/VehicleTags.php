7384cf0ef1969b03fdde3fcc8cbeb0266ba4fa12
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-12-28 16:37:04 -0700

    Componente: Vehicle Etiquetas (mostrará las etiquetas)

---
+++ app/Http/Livewire/VehicleTags.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class VehicleTags extends Component
+{
+    public function render()
+    {
+        return view('livewire.vehicle-tags');
+    }
+}
