b9ae4c36ba452c6c5978e000c312669d4b7868a4
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2023-01-09 10:46:51 -0700

    Se elmina indicativo TODO

--- resources/views/livewire/search/search-vehicles/search-vehicles.blade.php
+++ resources/views/livewire/search/search-vehicles/search-vehicles.blade.php
@@ -1,18 +1,5 @@
 <div>
-    {{-- TODO: Habilitar solo en caso de que sea necesario --}}
-    {{-- @if($show_only_favorites)
-        <div class="container">
-            <div class="text-right">
-                <button wire:click="$toggle('show_only_favorites')"
-                        class="btn btn-lg btn-success">
-                    {{ __('View All') }}
-                </button>
-            </div>
-        </div>
-
-    @endif --}}
-
-    <div class="vehicles-list">
+   <div class="vehicles-list">
         @foreach ($vehicles as $vehicle)
             @livewire('vehicle-card',['vehicle' => $vehicle],key($vehicle->id))
         @endforeach
