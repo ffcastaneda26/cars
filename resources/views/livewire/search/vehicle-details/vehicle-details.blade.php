118e61cf4d296070661de36e0d7a348eddfa1318
Federico Castaneda <ffcastaneda@gmail.com>
2023-02-26 14:07:24 -0600

    Detalles del Vehiculo se personaliza

--- resources/views/livewire/search/vehicle-details/vehicle-details.blade.php
+++ resources/views/livewire/search/vehicle-details/vehicle-details.blade.php
@@ -19,86 +19,22 @@
 
             <div class="row">
                 <div class="card-body">
-                
+
                 @include('livewire.search.commons.vehicle_title')
 
-                @include('livewire.search.commons.vehicle_body')
-          
-                <div class="d-flex justify-content-between align-items-start">
-                    @include('livewire.search.commons.vehicle_miles')
-                    @include('livewire.search.commons.vehicle_favorites')
-                </div>
-    
-                {{-- Etiquetas --}}
-                @include('livewire.search.commons.vehicle_tags')
-        
                 </div>
             </div>
 
             <div class="row">
                 <div class="card-footer text-muted">
-                    <div class="vehicle-alignleft">
-                        @livewire('add-contact-user',['vehicle' => $vehicle],key($vehicle->id))
-                    </div>
-                    
-                    @include('livewire.search.commons.vehicle_price')
+                    <p class="alinea-derecha">
+                        <strong>{{__('Stock Number')}} {{$vehicle->stock }}</strong>
+                    </p>
 
                 </div>
             </div>
 
-            <div class="row"> 
-                <div class="card-body"> 
-                    <div class="row">
-                            <div class="col-sm">
-                                <p class="text-capitalize"><strong>{{ __('Vin Number') }}</strong></p>
-                                <p>{{ $vehicle->vin }}</p>
-                                <strong>{{ __('Body') }}</strong>
-                                <p>{{ $vehicle->body }}</p>
-                                @if($vehicle->drive)
-                                    <strong>{{ __('Drive') }}</strong>
-                                    <p>{{ $vehicle->drive }}</p>
-                                @endif
-                                <strong>{{ __('TRANSMISSION') }}</strong>
-                                <p>{{ $vehicle->transmission }}</p>
-                                <strong>{{ __('ENGINE') }}</strong>
-                                {{-- <p>{{ $vehicle->engine_displacement/1000 }}L</p> --}}
-                                
-                                <p>{{  number_format(floatval($vehicle->engine_displacement/1000),2) }}L</p>
-                                <strong>{{ __('CYLINDERS') }}</strong>
-                                <p>{{ $vehicle->engine_cylinders }}</p>
-                                <strong>{{ __('FUEL TYPE') }}</strong>
-                                <p>{{ $vehicle->fuel_type_primary }}</p>
-                                <strong>{{ __('ENGINE POWER HP') }}</strong>
-                                <p>{{ $vehicle->engine_power_hp }}</p>
-                            </div>
 
-                            <div class="col-sm">
-                                <strong>{{ __('EXTERIOR COLOR') }}</strong>
-                                <p>{{ App::isLocale('en') ? $vehicle->exterior_color->english : $vehicle->exterior_color->spanish}}</p>
-                                <strong>{{ __('INTERIOR COLOR') }}</strong>
-                                <p>{{ App::isLocale('en') ? $vehicle->interior_color->english : $vehicle->interior_color->spanish}}</p>
-
-                                <strong>{{ __('INTERIOR MATERIAL') }}</strong>
-                                <p>{{ App::isLocale('en') ? $vehicle->material->english : $vehicle->material->spanish}}</p>
-
-                                @if($vehicle->number_of_rows)
-                                    <strong>{{ __('NUMBER OF ROWS') }}</strong>
-                                    <p>{{ $vehicle->number_of_rows }}</p>
-                                @endif
-
-                                <strong>{{ __('NUMBER OF SEATS') }}</strong>
-                                <p>{{ $vehicle->number_of_seats }}</p>
-                                <strong>{{ __('NUMBER OF DOORS') }}</strong>
-                                <p>{{ $vehicle->number_of_doors }}</p>
-                            
-                                @if($vehicle->series)
-                                    <strong>{{ ('SERIES') }}</strong>
-                                    <p>{{ $vehicle->series }}</p>
-                                @endif
-                            </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 </div>
