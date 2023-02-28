8d29ccdd00b2caede6d32dafcaa13148516c54c7
Federico Castaneda <ffcastaneda@gmail.com>
2023-02-28 15:06:04 -0600

    Carrousel de imagenes del vehiculo dentro de la tarjeta de ver mas

--- resources/views/livewire/search/show-carrousel-images.blade.php
+++ resources/views/livewire/search/show-carrousel-images.blade.php
@@ -1,20 +1,18 @@
-<div id="photo-{{$vehicle->id }}" class="carousel slide" data-bs-ride="carousel">
+<div id="carousel_vehicle_photos" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
-        @foreach ($vehicle->photos as $photo )
-            <div class="carousel-item active">
-                <img src="{{ asset('images/' .  $photo->path) }}" class="d-block w-100 photo-borders-superior">
-            </div>
+         @foreach ( $vehicle->photos as $photo )
+        <div class="carousel-item {{$loop->index == 0 ? 'active' : ''}}">
+            <img src="{{ asset('images/' .  $photo->path) }}" class="d-block w-100">
+        </div>
         @endforeach
-    </div>
-    @if($vehicle->photos->count())
-        <button class="carousel-control-prev" type="button"
-            data-bs-target="#photo-{{ $vehicle->id }}" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        </button>
 
-        <button class="carousel-control-next" type="button"
-            data-bs-target="#photo-{{ $vehicle->id }}" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        </button>
-    @endif
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carousel_vehicle_photos" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carousel_vehicle_photos" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
 </div>
